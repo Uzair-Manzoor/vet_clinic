3b4648ac3b42e4ee1420e41453417b37735ab8a4
Uzair-Manzoor <uzair.kayani555@gmail.com>
2023-10-28 13:59:35 +0500

    add query to calculate average escape_attempts

--- queries.sql
+++ queries.sql
@@ -1,13 +1,13 @@
-/*Queries that provide answers to the questions from all projects.*/
+/* Queries that provide answers to the questions from all projects. */
 
-SELECT * from animals WHERE name LIKE '%mon';
-SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
-SELECT name from animals WHERE escape_attempts < 3 AND neutered = true;
-SELECT date_of_birth from animals WHERE name IN ('Agumon', 'Pikachu') ;
-SELECT (name, escape_attempts) from animals WHERE weight_kg > 10.50;
-SELECT * from animals WHERE neutered = true;
-SELECT * from animals WHERE name != 'Gabumon';
-SELECT * from animals WHERE weight_kg BETWEEN 10.40 AND 17.30;
+SELECT * FROM animals WHERE name LIKE '%mon';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT name FROM animals WHERE escape_attempts < 3 AND neutered = true;
+SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.50;
+SELECT * FROM animals WHERE neutered = true;
+SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.40 AND 17.30;
 
 BEGIN;
 UPDATE animals SET species = 'unspecified';
@@ -30,9 +30,11 @@ ROLLBACK TO sp1;
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
 
+-- The new query to get the average escape_attempts for neutered and not neutered animals
+SELECT neutered, AVG(escape_attempts) FROM animals GROUP BY neutered;
 SELECT COUNT(*) FROM animals;
 SELECT COUNT(escape_attempts) FROM animals WHERE escape_attempts = 0;
 SELECT AVG(weight_kg) FROM animals;
 SELECT neutered, COUNT(escape_attempts) FROM animals GROUP BY neutered;
-SELECT species, MIN(weight_kg),MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
