662fea93c85229c387b2ce8f303b5d89f2a8c782
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-10-31 11:26:13 +0500

    insert data to increase execution time

--- data.sql
+++ data.sql
@@ -80,3 +80,4 @@ INSERT INTO visits VALUES
 ((SELECT id FROM animals WHERE name = 'Boarmon'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2020-08-03'),
 ((SELECT id FROM animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), '2020-05-24'),
 ((SELECT id FROM animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'William Tatcher'), '2021-01-11');
+((SELECT id FROM animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'William Tatcher'), '2021-01-11');
